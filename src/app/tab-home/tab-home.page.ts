4f93c04bb67dfe8255ec05dbb1ff296d03364928
TrungDuong <tuantrung1512@outlook.com>
2020-03-27 03:24:16 +1100

    implement slider in tab home

--- src/app/tab-home/tab-home.page.ts
+++ src/app/tab-home/tab-home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../services/menu.service';
+import { MenuItem } from '../models/classes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab-home',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabHomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private menuService: MenuService,
+    private router: Router
+  ) { }
+
+  items: MenuItem[] = [];
+  slideOpts = {
+    initialSlide: 0,
+    spaceBetween: -15,
+    centeredSlides: false,
+    slidesPerView: 2.5,
+    autoHeight: false
+  };
 
   ngOnInit() {
+
+    this.items = this.menuService.getMostPopular();
+
   }
 
+  goToDetail(categoryId: number, itemId: number) {
+    this.router.navigate(['tabs/menu', categoryId, itemId])
+  }
 }
