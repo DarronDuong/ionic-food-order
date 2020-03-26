853d42666ee665fdc946c520c14c756bf588dc50
TrungDuong <tuantrung1512@outlook.com>
2020-03-27 03:23:19 +1100

    add profile page

--- src/app/tab-more/tab-more-routing.module.ts
+++ src/app/tab-more/tab-more-routing.module.ts
@@ -7,11 +7,23 @@ const routes: Routes = [
   {
     path: '',
     component: TabMorePage
-  }
+  },
+  {
+    path: 'user-profile',
+    loadChildren: () => import('./pages/user-profile/user-profile.module').then(m => m.UserProfilePageModule)
+  },
+  {
+    path: 'location',
+    loadChildren: () => import('./pages/user-profile/user-profile.module').then(m => m.UserProfilePageModule)
+  },
+  {
+    path: 'credit-card-details',
+    loadChildren: () => import('./pages/credit-card-details/credit-card-details.module').then( m => m.CreditCardDetailsPageModule)
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TabMorePageRoutingModule {}
+export class TabMorePageRoutingModule { }
